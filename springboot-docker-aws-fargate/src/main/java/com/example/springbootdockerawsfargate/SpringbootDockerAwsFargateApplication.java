4a23be5645c501a002deb11f977c862a5ea0dcc7
Anupam95 <anupamsingla21@gmail.com>
2022-10-07 16:18:11 +0530

    First commit

---
+++ springboot-docker-aws-fargate/src/main/java/com/example/springbootdockerawsfargate/SpringbootDockerAwsFargateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerawsfargate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerAwsFargateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerAwsFargateApplication.class, args);
+	}
+
+}
---
+++ springboot-docker-aws-fargate/src/main/java/com/example/springbootdockerawsfargate/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.springbootdockerawsfargate.controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+    public String hello() {
+        String message = "Hello AWS!";
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            message += " From host: " + ip;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
+
+}
